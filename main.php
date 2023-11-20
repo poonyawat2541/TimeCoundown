4259575f23b10e4d7503fb88f46064289621ed26
poonyawat2541 <Poonyawatwongjaikam@gmail.com>
2023-11-20 19:50:20 +0700

    Create main.php

---
+++ main.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<!-- Coding by MultiWebPress /////// https://multiwebpress.com/ -->
+<html lang="en" dir="ltr">
+
+<head>
+  <meta charset="utf-8">
+  <title>ibank Application Moblie Banking | Time Countdown</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" charset="utf-8"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
+</head>
+
+<body>
+  <header>
+    <div class="inner-width">
+    </div>
+  </header>
+</body>
+
+</html>
+
+<head>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<div class="container">
+  <h1 id="headline">Ibank Application </h1>
+  <h2 id="headline"> Mobile Banking</h2>
+  <div id="countdown">
+    <ul>
+      <li><span id="days"></span>days</li>
+      <li><span id="hours"></span>Hours</li>
+      <li><span id="minutes"></span>Minutes</li>
+      <li><span id="seconds"></span>Seconds</li>
+    </ul>
+  </div>
+  <div id="content" class="emoji">
+    <span>🥳</span>
+    <span>🎉</span>
+    <span>🎂</span>
+  </div>
+
+</div>
+<script src="mo.js"> </script>
